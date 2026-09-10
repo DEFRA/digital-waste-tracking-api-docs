f99e1e25415b7e4bf9685e2efdb2f9b8325dcdfa
Dmitry Demenchuk <dmitry@demench.uk>
2026-09-10 15:26:58 +0100

    install prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/**
+ * @type {Options}
+ */
+export default {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  proseWrap: 'never'
+}
+
+/**
+ * @import { Options } from 'prettier'
+ */
